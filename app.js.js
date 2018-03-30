3606dcac51ffdff7ad8583ccaa6a390933e8ead6
MiswaJeremy <miswajeremy@gmail.com>
2018-03-30 23:56:04 +0300

    first commit

---
+++ app.js.js
@@ -0,0 +1,7 @@
+var user_input1=prompt("Please enter first input");
+var user_input2=propmt("Please enter second input");
+
+if(user_input1 != null && userinput2 != null){
+   document.getElementById("firstInput").innerHTML = userInput1;
+   console.log(userInput1);
+}
